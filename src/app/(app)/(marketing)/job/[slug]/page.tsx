6cecdd9ce20e9e5f54d602d63ba09f645a50cc45
charanm927 <charanm927@gmail.com>
2025-01-27 15:28:16 +0530

    chore: add nextjs page routes

---
+++ src/app/(app)/(marketing)/job/[slug]/page.tsx
@@ -0,0 +1,7 @@
+import JobDetails from '@/payload/blocks/Details/components/JobDetails'
+
+const page = () => {
+  return <JobDetails />
+}
+
+export default page
---
+++ src/app/(app)/(marketing)/jobs/page.tsx
@@ -0,0 +1,7 @@
+import JobsList from '@/payload/blocks/List/components/JobsList'
+
+const page = () => {
+  return <JobsList />
+}
+
+export default page
